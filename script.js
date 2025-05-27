338e76bb7cb1501f3fe85981bd377b4835b3c5b7
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-05-27 16:52:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let o={};
+	for (let l of str) {
+		if(str[l])
+		{
+			o[l]++;
+		}
+		else
+		{
+			o[l]=1;
+		}
+	}
+	for (let l of str) {
+		if(o[l]==1)
+		{
+			return l;
+		}
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
